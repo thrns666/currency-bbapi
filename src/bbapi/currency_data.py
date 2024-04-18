e491f8362b1ea7c602e9946a58bc0753f1d58f12
thrns666 <super.tawer@gmail.com>
2024-04-18 16:52:54 +0300

    bbapi ***

--- src/bbapi/currency_data.py
+++ src/bbapi/currency_data.py
@@ -1,11 +1,79 @@
 import aiohttp
 
 
+json_data = [
+    {
+        "USD_in": "3.2650", "USD_out": "3.3050", "EUR_in": "3.4600", "EUR_out": "3.5300", "RUB_in": "3.4300",
+        "RUB_out": "3.4900", "GBP_in": "0.0000", "GBP_out": "0.0000", "CAD_in": "0.0000", "CAD_out": "0.0000",
+        "PLN_in": "0.0000", "PLN_out": "0.0000", "SEK_in": "0.0000", "SEK_out": "0.0000", "CHF_in": "0.0000",
+        "CHF_out": "0.0000", "USD_EUR_in": "0.9270", "USD_EUR_out": "1.0600", "USD_RUB_in": "93.6000",
+        "USD_RUB_out": "0.0104", "RUB_EUR_in": "0.0098", "RUB_EUR_out": "99.2000", "JPY_in": "0.0000",
+        "JPY_out": "0.0000", "CNY_in": "4.5200", "CNY_out": "4.6200", "CNY_EUR_in": "0.0000",
+        "CNY_EUR_out": "0.0000", "CNY_USD_in": "0.1370", "CNY_USD_out": "7.0800", "CNY_RUB_in": "0.0000",
+        "CNY_RUB_out": "0.0000", "CZK_in": "0.0000", "CZK_out": "0.0000", "NOK_in": "0.0000", "NOK_out": "0.0000",
+        "filial_id": "1246", "sap_id": "50000081",
+        "info_worktime": "Пн |Вт 10 00 19 00 |Ср 10 00 19 00 |Чт 10 00 19 00 |Пт 10 00 19 00 |Сб 10 00 16 00 |Вс |",
+        "street_type": "бул.", "street": "Шевченко", "filials_text": "Отделение 510\/173", "home_number": "12",
+        "name": "Минск", "name_type": "г."
+        },
+    {
+        "USD_in": "3.2650", "USD_out": "3.3050", "EUR_in": "3.4600", "EUR_out": "3.5300", "RUB_in": "3.4300",
+        "RUB_out": "3.4900", "GBP_in": "0.0000", "GBP_out": "0.0000", "CAD_in": "0.0000", "CAD_out": "0.0000",
+        "PLN_in": "0.0000", "PLN_out": "0.0000", "SEK_in": "0.0000", "SEK_out": "0.0000", "CHF_in": "0.0000",
+        "CHF_out": "0.0000", "USD_EUR_in": "0.9270", "USD_EUR_out": "1.0600", "USD_RUB_in": "93.6000",
+        "USD_RUB_out": "0.0104", "RUB_EUR_in": "0.0098", "RUB_EUR_out": "99.2000", "JPY_in": "0.0000",
+        "JPY_out": "0.0000", "CNY_in": "4.5200", "CNY_out": "4.6200", "CNY_EUR_in": "0.0000",
+        "CNY_EUR_out": "0.0000", "CNY_USD_in": "0.1370", "CNY_USD_out": "7.0800", "CNY_RUB_in": "0.0000",
+        "CNY_RUB_out": "0.0000", "CZK_in": "0.0000", "CZK_out": "0.0000", "NOK_in": "0.0000", "NOK_out": "0.0000",
+        "filial_id": "1246", "sap_id": "50000081",
+        "info_worktime": "Пн |Вт 10 00 19 00 |Ср 10 00 19 00 |Чт 10 00 19 00 |Пт 10 00 19 00 |Сб 10 00 16 00 |Вс |",
+        "street_type": "бул.", "street": "Центральная", "filials_text": "Отделение 110\/122", "home_number": "33",
+        "name": "Пинск", "name_type": "г."
+        }
+    ]
+
+
+def separate_filial(data_json: list, a: bool = True) -> dict:
+
+    cur_in = {}
+    cur_out = {}
+    service_info = {}
+    result = {}
+
+    for data in data_json:
+        a = True
+        cur_in = {}
+        cur_out = {}
+        service_info = {}
+        result = {}
+        for name, cost in data.items():
+            if name != 'filial_id' and a:
+                if name.split('_')[-1] == 'in':
+                    cur_in[f'{name}'] = cost
+                elif name.split('_')[-1] == 'out':
+                    cur_out[f'{name}'] = cost
+            elif name == 'filial_id':
+                a = False
+            elif not a:
+                service_info[name] = cost
+
+        result[data['filial_id'] + ' ' + data['name']] = {
+            'in': cur_in,
+            'out': cur_out,
+            'info': service_info
+        }
+    return result
+
+
+a = separate_filial(json_data)
+print(a)
+
+
 async def get_currency():
     # BelaBank api url here
-    url = ''
+    url = ''  # 'https://belarusbank.by/api/kursExchange'
 
     with aiohttp.ClientSession() as session:
         response = await session.get(url=url)
 
-    return response
+    return separate_filial(response.json())
--- src/posts/router.py
+++ src/posts/router.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from starlette.requests import Request
 from fastapi.templating import Jinja2Templates
+from typing_extensions import Annotated
+
 from src.bbapi.currency_data import get_currency
 
 main_router = APIRouter()
@@ -20,6 +22,20 @@ async def index(request: Request):
 
 
 @main_router.post('/index')
-async def take_data(request: Request, office: str, currency_in: str, currency_out: str):
-    print(office, currency_in, currency_out)
-    return 'ok 201'
+async def take_data(
+        request: Request,
+        office: Annotated[str, Form()],
+        currency_in: Annotated[str, Form()],
+        currency_out: Annotated[str, Form()],
+        cash_in: Annotated[str, Form()],
+        cash_out: Annotated[str, Form()]
+):
+    print(office, currency_in, currency_out, cash_in, cash_out)
+
+    sum = int(cash_in) + int(cash_out)
+    print("total = ", sum)
+    context = {
+        'total': sum
+    }
+
+    return templates.TemplateResponse(request=request, name='index.html', context=context, status_code=200)
